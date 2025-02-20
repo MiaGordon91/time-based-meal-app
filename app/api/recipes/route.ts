b58ee832f578b78a0342d19c5e56a228b73086d0
Tris <tristolliday@monzo.com>
2025-02-20 18:04:13 +0000

    sanitise queries in route

--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -1,34 +1,55 @@
 import postgres from "postgres";
 import { NextResponse } from "next/server";
 
-if(!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL env variable is not set");
+if (!process.env.DATABASE_URL || typeof process.env.DATABASE_URL !== "string") {
+  throw new Error("DATABASE_URL env variable is not set");
 }
 
-const sql: postgres.Sql = postgres(process.env.DATABASE_URL);
-  
-export async function GET(req: Request){
+const databaseUrl = process.env.DATABASE_URL;
 
-    try {
-        const {searchParams} = new URL(req.url); // creates a URL object that's destructured to access searchParams property
-        const dietary = searchParams.get("dietary")?.split(",") || [];
-        const time = searchParams.get("time");     
+const sql = postgres(databaseUrl);
 
-        console.log("Received params:", { dietary, time });
+const dietaryOptions = ["lactoseIntolerant", "vegetarian", "vegan"] as const;
+const timeOptions = ["0", "15", "30", "45", "60"] as const;
 
-        const response = await sql`
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url); // creates a URL object that's destructured to access searchParams property
+
+    let time = "20";
+
+    const dietaryQuery = searchParams.get("dietary");
+
+    const dietaryArray = dietaryQuery?.split(",").filter((dietary) => {
+      return dietary in dietaryOptions;
+    }) || ["none"];
+
+    const timeQuery = searchParams.get("time");
+
+    if (timeQuery != null && timeQuery in timeOptions) {
+      time = timeQuery;
+    } else {
+      // eslint-disable-next-line no-console
+      console.error("Invalid time query:", timeQuery);
+    }
+
+    // const dietary = searchParams.get("dietary")?.split(",") || [];
+    // const time = searchParams.get("time");
+
+    // console.log("Received params:", { dietary, time });
+
+    const response = await sql`
             SELECT * FROM recipes  
-            WHERE ${dietary.length > 0 ? sql`dietary && ${sql.array(dietary)}` : sql`TRUE`}
-            AND ${time ? sql`time = ${time}` : sql`TRUE`}
+            WHERE dietary && ${sql.array(dietaryArray)}
+            AND time = ${time}
             ORDER BY id DESC;
         `;
 
-        // Raw SQL Translation example => 
-        // SELECT * FROM recipes WHERE dietary && ARRAY['lactoseIntolerant', 'vegetarian'] AND time = '30';`;
+    // Raw SQL Translation example =>
+    // SELECT * FROM recipes WHERE dietary && ARRAY['lactoseIntolerant', 'vegetarian'] AND time = '30';`;
 
-        return NextResponse.json(response);
-    } catch (error) {
-        return NextResponse.json({error: "Database error"}, {status: 500});
-    }
+    return NextResponse.json(response);
+  } catch (error) {
+    return NextResponse.json({ error: "Database error" }, { status: 500 });
+  }
 }
-    
